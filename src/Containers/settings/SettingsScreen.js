a69f5f680667472527801829951606a245107bd6
wlcli <wlc.licheng@gmail.com>
2020-07-13 13:40:26 +0800

    Updated Settings Page UI

--- src/Containers/settings/SettingsScreen.js
+++ src/Containers/settings/SettingsScreen.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { StyleSheet, View, Text, Alert } from "react-native";
-import { Container, Header, Title, Content, List, Separator, ListItem } from "native-base";
+import { StyleSheet, View, Text, Alert, Dimensions } from "react-native";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  List,
+  Separator,
+  ListItem,
+  Left,
+  Body,
+} from "native-base";
 import firebase from "../../../firebaseDb";
-import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import MonthlyTargetModal from "./MonthlyTargetModal"
+import MonthlyTargetModal from "./MonthlyTargetModal";
 
 const Settings = () => {
   const navigation = useNavigation();
@@ -21,16 +31,18 @@ const Settings = () => {
           onPress: handleLogOutUser,
           style: "destructive",
         },
-        { text: "Cancel" }
+        { text: "Cancel" },
       ],
       {
         cancelable: true,
       }
-    )
-  }
+    );
+  };
 
   const handleLogOutUser = () => {
-    firebase.auth().signOut()
+    firebase
+      .auth()
+      .signOut()
       .then(() => {
         // Sign-out successful.
         console.log("Signed out");
@@ -39,44 +51,50 @@ const Settings = () => {
         // An error happened.
         console.log(error.toString());
       });
-  }
+  };
 
   const confirmDeleteAccount = () => {
     Alert.alert(
       "Delete account",
-      "Are you sure you want to delete your account? " + 
-      "This action is irreversible and you will lose all your data.",
+      "Are you sure you want to delete your account? " +
+        "This action is irreversible and you will lose all your data.",
       [
         {
           text: "Delete",
           onPress: handleDeleteAccount,
           style: "destructive",
         },
-        { text: "Cancel" }
+        { text: "Cancel" },
       ],
       {
         cancelable: true,
       }
-    )
-  }
+    );
+  };
 
   const handleDeleteAccount = () => {
     const uid = firebase.auth().currentUser.uid;
     handleDeleteData(uid);
-    firebase.auth().currentUser.delete()
+    firebase
+      .auth()
+      .currentUser.delete()
       .then(() => {
         console.log("Account deleted");
       })
-      .catch(error => console.error(error));
-  }
+      .catch((error) => console.error(error));
+  };
 
   // Realised it does not work because deleting doc does not delete its subcollection
-  const handleDeleteData = uid => {
+  const handleDeleteData = (uid) => {
     console.log(uid);
-    firebase.firestore().collection("Users").doc(uid).delete()
+    firebase
+      .firestore()
+      .collection("Users")
+      .doc(uid)
+      .delete()
       .then(() => console.log("User data deleted"))
-      .catch(error => console.error(error));
-  }
+      .catch((error) => console.error(error));
+  };
 
   return (
     // {/* <Ionicons
@@ -85,53 +103,86 @@ const Settings = () => {
     //     size={40}
     //     onPress={signOut}
     //   /> */}
-    <Container style={styles.container} >
-      <Header style={styles.header}>
-        <Title style={styles.title}>Settings</Title>
-      </Header>
-      <Content>
-        <List>
-          <Separator style={styles.separator} >
-            <Text style={styles.separatorText}>General</Text>
-          </Separator>
-          <ListItem key="profile" noBorder style={styles.item}>
-            <Text style={styles.itemText}>Profile</Text>
-          </ListItem>
+    <Container style={styles.container}>
+      <View style={styles.headerAndListContainer}>
+        <View style={styles.header}>
+          <Title style={styles.title}>Settings</Title>
+        </View>
 
-          <Separator style={styles.separator} >
-            <Text style={styles.separatorText}>Manage</Text>
-          </Separator>
-          <ListItem key="budget" noBorder style={styles.item}
-            onPress={() => setModalVisible(!modalVisible)}
-          >
-            <Text style={styles.itemText}>Monthly Budget</Text>
-          </ListItem>
-          <ListItem key="category" noBorder style={styles.item}
-            onPress={() => navigation.navigate("CategoryScreen")}
-          >
-            <Text style={styles.itemText}>Categories</Text>
-          </ListItem>
+        <View style={styles.containerView}>
+          <List style={styles.listContainerView}>
+            <View>
+              <View style={styles.separator}>
+                <Text style={styles.separatorText}>General</Text>
+              </View>
+              <ListItem key="profile" noBorder style={styles.item}>
+                <Text style={styles.itemText}>Profile</Text>
+              </ListItem>
+            </View>
 
-          <Separator style={styles.separator} >
-            <Text style={styles.separatorText}>Account</Text>
-          </Separator>
-          <ListItem key="logout" noBorder style={styles.item}
-            onPress={confirmLogOut}
-          >
-            <Text style={styles.itemText}>Logout</Text>
-          </ListItem>
-          <ListItem key="delete" noBorder style={styles.item}
-            onPress={confirmDeleteAccount}
-          >
-            <Text style={styles.itemText}>Delete Account</Text>
-          </ListItem>
-        </List>
-      </Content>
+            <View>
+              <View style={styles.separator}>
+                <Text style={styles.separatorText}>Manage</Text>
+              </View>
+              <ListItem
+                icon
+                key="budget"
+                noBorder
+                style={styles.item}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Left>
+                  <AntDesign name="barchart" size={24} color="#0E7FFF" />
+                </Left>
+                <Body>
+                  <Text style={styles.itemText}>Monthly Budget</Text>
+                </Body>
+              </ListItem>
+              <ListItem
+                icon
+                key="category"
+                noBorder
+                style={styles.item}
+                onPress={() => navigation.navigate("CategoryScreen")}
+              >
+                <Left>
+                  <AntDesign name="piechart" size={24} color="#0E7FFF" />
+                </Left>
+                <Body>
+                  <Text style={styles.itemText}>Categories</Text>
+                </Body>
+              </ListItem>
+            </View>
+
+            <View>
+              <View style={styles.separator}>
+                <Text style={styles.separatorText}>Account</Text>
+              </View>
+              <ListItem
+                key="logout"
+                noBorder
+                style={styles.item}
+                onPress={confirmLogOut}
+              >
+                <Text style={styles.itemText}>Logout</Text>
+              </ListItem>
+              <ListItem
+                key="delete"
+                noBorder
+                style={styles.item}
+                onPress={confirmDeleteAccount}
+              >
+                <Text style={styles.itemText}>Delete Account</Text>
+              </ListItem>
+            </View>
+          </List>
+        </View>
+      </View>
       <MonthlyTargetModal
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
       />
-    </Container >
+    </Container>
   );
 };
 
@@ -140,31 +191,75 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#e2eeff",
   },
+  headerAndListContainer: {
+    flex: 1,
+    zIndex: 1,
+    flexDirection: "row",
+    backgroundColor: "#EFEFEF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   header: {
-    height: 60,
-    backgroundColor: "#e2eeff"
+    height: 200,
+    width: Dimensions.get("window").width,
+    alignSelf: "flex-start",
+    // alignContent: "center",
+    justifyContent: "center",
+    paddingLeft: 20,
+    backgroundColor: "#0E7FFF",
+    zIndex: 2,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
+    marginLeft: 5,
+  },
+  containerView: {
+    flexDirection: "column",
+    backgroundColor: "white",
+    zIndex: 3,
+    position: "absolute",
+    justifyContent: "space-between",
+    // alignSelf: "center",
+    height: 450,
+    width: 380,
+    shadowColor: "#000",
+    borderRadius: 20,
+    shadowOpacity: 0.25,
+    shadowRadius: 3.8,
+    elevation: 5,
+  },
+  listContainerView: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-start",
   },
   separator: {
-    height: 40,
-    backgroundColor: "#e2eeff"
+    // height: -10,
+    width: 100,
+    padding: -10,
+    marginLeft: 10,
+    marginTop: 10,
+    // backgroundColor: "white",
   },
   separatorText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "darkblue",
-    marginTop: 8
+    fontSize: 12,
+    // fontWeight: "bold",
+    color: "grey",
+    marginTop: 8,
+    marginLeft: 10,
   },
   item: {
     height: 40,
-    opacity: 0.6
+    margin: 5,
+    marginLeft: 30,
+    // opacity: 0.6,
   },
   itemText: {
-    fontSize: 15,
-    marginTop: -5
-  }
+    fontSize: 16,
+    // fontWeight: "800",
+    color: "#2B2B2B",
+    marginTop: -5,
+  },
 });
 
 export default Settings;
